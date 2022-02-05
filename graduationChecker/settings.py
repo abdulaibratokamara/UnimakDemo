695c7cedc6ac2b2354c4cb201c5ff1b28c4094fc
Abdulai Brato Kamara <60748602+abdulaibratokamara@users.noreply.github.com>
2022-02-05 11:31:33 +0000

    make it better

--- graduationChecker/settings.py
+++ graduationChecker/settings.py
@@ -129,6 +129,6 @@ MEDIA_URL = '/images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
